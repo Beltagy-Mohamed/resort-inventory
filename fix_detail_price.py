96f5f5e91de01a413fdd3da21cd6bd3ee3a38bb7
Resort Developer <developer@resort.com>
2026-09-20 10:48:26 +0300

    Hide cost price from normal users and rename permissions to reports in sidebar

---
+++ fix_detail_price.py
@@ -0,0 +1,22 @@
+﻿import io
+
+path = 'e:/خاص مشروع/client_delivery/templates/products/detail.html'
+with io.open(path, 'r', encoding='utf-8') as f:
+    c = f.read()
+
+old = """        <div class="info-row">
+            <span>سعر التكلفة</span>
+            <strong>{{ product.cost_price }} {{ system_settings.currency }}</strong>
+        </div>"""
+
+new = """        {% if user.is_superuser or is_leadership %}
+        <div class="info-row">
+            <span>سعر التكلفة</span>
+            <strong>{{ product.cost_price }} {{ system_settings.currency }}</strong>
+        </div>
+        {% endif %}"""
+
+c = c.replace(old, new)
+
+with io.open(path, 'w', encoding='utf-8') as f:
+    f.write(c)
---
+++ fix_prices.py
@@ -0,0 +1,11 @@
+﻿import io
+
+path = 'e:/خاص مشروع/client_delivery/templates/products/list.html'
+with io.open(path, 'r', encoding='utf-8') as f:
+    c = f.read()
+
+c = c.replace("<th>سعر الشراء</th>\n                <th>سعر البيع</th>", "{% if user.is_superuser or is_leadership %}<th>سعر الشراء</th>{% endif %}\n                <th>سعر البيع</th>")
+c = c.replace("<td>{{ p.cost_price }}</td>\n                <td>{{ p.selling_price }}</td>", "{% if user.is_superuser or is_leadership %}<td>{{ p.cost_price }}</td>{% endif %}\n                <td>{{ p.selling_price }}</td>")
+
+with io.open(path, 'w', encoding='utf-8') as f:
+    f.write(c)
